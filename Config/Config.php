429b598f010b77e86396b2a807c36e38d53bd822
cdquinterop <darmiqp@gmail.com>
2022-08-23 16:20:13 -0500

    Envio de Gmail

---
+++ Config/Config.php
@@ -0,0 +1,32 @@
+<?php 
+	
+	//define("BASE_URL", "http://localhost/tienda_virtual/");
+	const BASE_URL = "http://localhost:8080/project_ecommerce/";
+
+	//Zona horaria
+	date_default_timezone_set('America/Guatemala');
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "db_ecommerce";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "utf8";
+
+	//Deliminadores decimal y millar Ej. 24,1989.00
+	const SPD = ".";
+	const SPM = ",";
+
+	//Simbolo de moneda
+	const SMONEY = "Q";
+
+	//Datos envio de correo
+	const NOMBRE_REMITENTE = "EASY TECH";
+	const EMAIL_REMITENTE = "darmiqp@gmail.com";
+	const NOMBRE_EMPESA = "EASY TECH";
+	const WEB_EMPRESA = "https://easytech.com.ec";
+
+	
+
+
+ ?>
